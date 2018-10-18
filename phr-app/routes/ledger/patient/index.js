c6abe744231cf9e1b1898c24b70cb0bc8b4c2598
Sarah Bacha <sarah.bacha94@gmail.com>
2018-10-18 14:48:45 +0300

    update

--- phr-app/routes/ledger/patient/index.js
+++ phr-app/routes/ledger/patient/index.js
@@ -4,13 +4,15 @@ const router = require('express').Router();
 module.exports = function (app) {
   var controller = require('./patient.route.controller.js')(app);
   router.get('/:id', controller.getPatientbyID);
+
   router.get('/general/:id', controller.getPatientGeneralInformation);
-  router.get('/diabetes/:id', controller.getPatientDiabetesIndications);
   router.post('/general', controller.createPatientGeneralInformation);
-  router.post('/diabetes', controller.createPatientDiabetesIndications);
   router.patch('/general/:id', controller.updatePatientGeneralInformation);
-  router.patch('/diabetes/:id', controller.updatePatientDiabetesIndications);
   router.get('/generalhistory/:id', controller.getPatientGeneralInformationHistory);
+  
+  router.get('/diabetes/:id', controller.getPatientDiabetesIndications);
+  router.post('/diabetes', controller.createPatientDiabetesIndications);
+  router.patch('/diabetes/:id', controller.updatePatientDiabetesIndications);
   router.get('/diabeteshistory/:id', controller.getPatientDiabetesIndicationsHistory);
   return router;
 };
